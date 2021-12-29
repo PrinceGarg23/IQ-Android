599b9991256884f4cd2d17ac63d34cb34ae54624
PrinceGarg23 <princegarg230@gmail.com>
2021-12-30 02:10:45 +0530

    Added About us fragment Content

--- app/src/main/java/com/example/iqapp/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/iqapp/ui/about/AboutFragment.java
@@ -1,9 +1,12 @@
 package com.example.iqapp.ui.about;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,12 +18,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.iqapp.R;
 import com.example.iqapp.databinding.AboutFragmentBinding;
 
 public class AboutFragment extends Fragment {
 
     private AboutViewModel aboutViewModel;
     private AboutFragmentBinding binding;
+
+    private ConstraintLayout contact,linkedin,facebook,instagram,twitter,board;
+
+    private static final String EMAIL = "contact.iquestvit@gmail.com";
+    private static final String FACEBOOK = "https://www.facebook.com/InnovatorsQuest/";
+    private static final String TWITTER = "https://twitter.com/innovatorsvit/";
+    private static final String INSTAGRAM = "https://www.instagram.com/iquest.vit/";
+    private static final String LINKEDIN = "https://www.linkedin.com/company/innovatorsquest/";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,9 +47,66 @@ public class AboutFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        contact = getView().findViewById(R.id.contactus);
+        linkedin = getView().findViewById(R.id.linkedinSettings);
+        facebook = getView().findViewById(R.id.facebookSettings);
+        instagram = getView().findViewById(R.id.instagramSettings);
+        twitter = getView().findViewById(R.id.twitterSettings);
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_SUBJECT,"");
+                intent.putExtra(Intent.EXTRA_TEXT,"");
+                startActivity(intent);
+            }
+        });
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(LINKEDIN);
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(FACEBOOK);
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(TWITTER);
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(INSTAGRAM);
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+
     }
 
 }
--- app/src/main/java/com/example/iqapp/ui/about/AboutViewModel.java
+++ app/src/main/java/com/example/iqapp/ui/about/AboutViewModel.java
@@ -1,19 +1,15 @@
 package com.example.iqapp.ui.about;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.iqapp.R;
+
 public class AboutViewModel extends ViewModel {
     // TODO: Implement the ViewModel
-    private MutableLiveData<String> mText;
 
     public AboutViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is About fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
     }
 }
